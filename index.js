95c7860dfd924ff73b07cff2f4c78b11cb22355a
Paulo Ferreira <paulo.ferreira25@gmail.com>
2018-07-23 08:30:42 -0300

    v1

--- index.js
+++ index.js
@@ -26,25 +26,180 @@ module.exports = {
       throw error
     }
   },
-  insert: async function (dataBase, collectionName, doc) {
+  create: async function (dataBase, collectionName, doc) {
     try {
+      let insetOptions = {
+        waitForSync: true,
+        returnNew: true
+      }
       let db = await this.getDB()
       db.useDatabase(dataBase)
       let collection = db.collection(collectionName)
-      return await collection.save(doc)
+      let newDoc = await collection.save(doc, insetOptions)
+      return newDoc.new
     } catch (error) {
       if (error.message.indexOf('database not found') >= 0) {
         await this.createDB(dataBase)
-        return this.insert(dataBase, collectionName, doc)
+        return this.create(dataBase, collectionName, doc)
       } else if (error.message.indexOf('collection not found') >= 0) {
         await this.createCollection(dataBase, collectionName)
-        return this.insert(dataBase, collectionName, doc)
+        return this.create(dataBase, collectionName, doc)
       } else {
         console.log(error)
         throw error
       }
     }
   },
+  update: async function (dataBase, collectionName, bindVars, newValue) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#update-by-example
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      return await collection.updateByExample(bindVars, newValue, true, true)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  },
+  updateByID: async function (dataBase, collectionName, bindVars, newValue) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#update
+    try {
+      let options = {
+        waitForSync: true,
+        returnNew: true
+      }
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      return await collection.update(bindVars, newValue, options)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  },
+  replace: async function (dataBase, collectionName, bindVars, newValue) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#replace-by-example
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      return await collection.replaceByExample(bindVars, newValue, true)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  },
+  replaceByID: async function (dataBase, collectionName, bindVars, newValue) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#replace
+    try {
+      let options = {
+        waitForSync: true,
+        returnNew: true
+      }
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      return await collection.replace(bindVars, newValue, options)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  },
+  delete: async function (dataBase, collectionName, bindVars) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#remove-by-example
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      return await collection.removeByExample(bindVars, true)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  },
+  deleteByID: async function (dataBase, collectionName, key) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#remove
+    try {
+      let options = {
+        waitForSync: true
+      }
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      return await collection.remove(key, options)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  },
+  findOne: async function (dataBase, collectionName, bindVars) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#query-by-example
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      let cursor = await collection.byExample(bindVars)
+      if (cursor.count >= 1) {
+        return cursor._result[0]
+      } else {
+        let msg = `Resultado não encontrado. DB[${dataBase}]/collection[${collectionName}]/bindVars[${JSON.stringify(bindVars)}]`
+        let error = new Error(msg)
+        console.log(error)
+        throw error
+      }
+    } catch (error) {
+      throw error
+    }
+  },
+  find: async function (dataBase, collectionName, bindVars) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#query-by-example
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      let cursor = await collection.byExample(bindVars)
+      return await cursor.all()
+    } catch (error) {
+      throw error
+    }
+  },
+  findByID: async function (dataBase, collectionName, bindVars) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#document
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      let cursor = await collection.document(bindVars)
+      return cursor
+    } catch (error) {
+      throw error
+    }
+  },
+  count: async function (dataBase, collectionName, bindVars) {
+    // https://docs.arangodb.com/3.3/Manual/DataModeling/Documents/DocumentMethods.html#count
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let collection = db.collection(collectionName)
+      let cursor = await collection.count()
+      return cursor.count
+    } catch (error) {
+      throw error
+    }
+  },
+  query: async function (dataBase, query, bindVars) {
+    //  https://docs.arangodb.com/3.0/AQL/Fundamentals/Syntax.html
+    try {
+      let db = await this.getDB()
+      db.useDatabase(dataBase)
+      let cursor = await db.query({ query: query, bindVars: bindVars })
+      return await cursor.all()
+    } catch (error) {
+      throw error
+    }
+  },
+
   init: async function (config) {
     try {
       if (typeof config === 'undefined') {
--- test/src/run.js
+++ test/src/run.js
@@ -1,25 +1,99 @@
 const expect = require('chai').expect
+const chai = require('chai')
+const assertArrays = require('chai-arrays')
+chai.use(assertArrays)
 const arangodb = require('../../index')
 const dataBase = 'testDB'
 const collectionName = 'testCollectionName'
-let doc
+let doc1
+let doc2
+let doc3
 describe('run', function () {
-  it('insert doc', async function () {
-    let docInsert = {
-      a: 1,
-      b: 2
-    }
-    let addDoc = await arangodb.insert(dataBase, collectionName, docInsert)
-    doc = addDoc
-    expect(doc).to.be.a('object')
+  it('insert doc 1', async function () {
+    let docInsert = { a: 1, b: 2, c: 3 }
+    let addDoc = await arangodb.create(dataBase, collectionName, docInsert)
+    doc1 = addDoc
+    expect(doc1).to.be.a('object')
+  })
+  it('insert doc 2', async function () {
+    let docInsert = { a: 1, b: 2, c: 4 }
+    let addDoc = await arangodb.create(dataBase, collectionName, docInsert)
+    doc2 = addDoc
+    expect(doc2).to.be.a('object')
+  })
+  it('insert doc 3', async function () {
+    let docInsert = { j: 1 }
+    let addDoc = await arangodb.create(dataBase, collectionName, docInsert)
+    doc3 = addDoc
+    expect(doc3).to.be.a('object')
+  })
+  it('findOne', async function () {
+    let bindVars = { a: 1, b: 2, c: 3 }
+    let findDoc = await arangodb.findOne(dataBase, collectionName, bindVars)
+    expect(findDoc).to.deep.equal(doc1)
+  })
+  it('find', async function () {
+    let bindVars = { a: 1, b: 2 }
+    let findDocs = await arangodb.find(dataBase, collectionName, bindVars)
+    expect(findDocs).to.be.array()
+  })
+  it('find By ID - Object', async function () {
+    let bindVars = { _id: doc1._id }
+    let findDoc = await arangodb.findByID(dataBase, collectionName, bindVars)
+    expect(findDoc).to.deep.equal(doc1)
+  })
+  it('find By ID - String', async function () {
+    let bindVars = doc1._id
+    let findDoc = await arangodb.findByID(dataBase, collectionName, bindVars)
+    expect(findDoc).to.deep.equal(doc1)
+  })
+  it('find By Key', async function () {
+    let bindVars = { _key: doc1._key }
+    let findDoc = await arangodb.findByID(dataBase, collectionName, bindVars)
+    expect(findDoc).to.deep.equal(doc1)
   })
   it('update doc', async function () {
-    let docInsert = {
-      a: 1,
-      b: 2
-    }
-    let addDoc = await arangodb.insert(dataBase, collectionName, docInsert)
-    doc = addDoc
-    expect(doc).to.be.a('object')
+    let bindVars = { a: 1, c: 3 }
+    let newValue = { a: 2, d: 2 }
+    let result = await arangodb.update(dataBase, collectionName, bindVars, newValue)
+    expect(result).to.include({ updated: 1 })
+  })
+  it('update By ID', async function () {
+    let bindVars = doc3._id
+    let newValue = { k: 2, m: 2 }
+    let result = await arangodb.updateByID(dataBase, collectionName, bindVars, newValue)
+    expect(result).to.be.a('object')
+  })
+  it('replace doc', async function () {
+    let bindVars = { a: 2, d: 2 }
+    let newValue = { a: 1, x: 5 }
+    let result = await arangodb.replace(dataBase, collectionName, bindVars, newValue)
+    expect(result).to.include({ replaced: 1 })
+  })
+  it('replace By ID', async function () {
+    let bindVars = doc3._id
+    let newValue = { a: 1, x: 5 }
+    let result = await arangodb.replaceByID(dataBase, collectionName, bindVars, newValue)
+    expect(result).to.be.a('object')
+  })
+  it('count', async function () {
+    let result = await arangodb.count(dataBase, collectionName)
+    expect(result).to.be.a('number')
+  })
+  it('query', async function () {
+    let bindVars = { a: 1 }
+    let query = `FOR d in ${collectionName} FILTER d.a == @a RETURN d`
+    let result = await arangodb.query(dataBase, query, bindVars)
+    expect(result).to.be.a('array')
+  })
+  it('delete By ID', async function () {
+    let bindVars = doc3._id
+    let result = await arangodb.deleteByID(dataBase, collectionName, bindVars)
+    expect(result).to.be.a('object')
+  })
+  it('delete', async function () {
+    let bindVars = { a: 1 }
+    let result = await arangodb.delete(dataBase, collectionName, bindVars)
+    expect(result).to.include({ deleted: 2 })
   })
 })
